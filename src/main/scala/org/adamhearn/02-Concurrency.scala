72ffb9c0107f3865967f95d41a907c0bc5ab98c8
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-11-06 19:30:36 +0100

    structure

--- src/main/scala/org/adamhearn/02-Concurrency.scala
+++ src/main/scala/org/adamhearn/02-Concurrency.scala
@@ -9,18 +9,58 @@ import zio.test.TestAspect.ignore
 import zio.test.TestResult
 import java.io.IOException
 
+/** Async effect allows for the asynchronous execution of computations via a managed thread pool.
+  *
+  * The core function, run, spawns a new "green thread," also known as a fiber, to handle the given
+  * computation.
+  */
 object `02_Async` extends KyoSpecDefault {
-  def spec = suite("primitives")(
-    test("concurrent") {
+  def spec = suite("Async")(
+    suite("Fiber")(
+      test("fork") {
+        // TODO: show how to make a fiber directly
+        assertCompletes
+      },
+      test("join") {
+        // show how to `.get` on a fiber
+        // runs 'synchronously' for all maps
+        assertCompletes
+      },
+    ),
+    test("run") {
+
+      /** Exercise: Async.run
+        *
+        * TODO: talk about Async and how it's not a single effect TODO: talk about 'forking' with
+        * multiple pending effects `Async.run(_)
+        */
       assertCompletes
-    }
+    },
+    test("race") {
+      assertCompletes
+    },
+    test("timeout") {
+      assertCompletes
+    },
+    test("local") {
+      assertCompletes
+    },
   )
 }
 object `02_Structures` extends KyoSpecDefault {
   def spec =
     suite("structures")(
-      test("concurrent") {
-        assertCompletes
-      }
+      suite("Atomic")(
+        test("Ref") {
+          assertCompletes
+        }
+      ),
+      suite("Queue")(
+        test("bounded") {
+          // TODO: show semantics of bounded queue
+          // Maybe fork a fiber and then use a separate fiber to take from the queue
+          assertCompletes
+        }
+      ),
     )
 }
